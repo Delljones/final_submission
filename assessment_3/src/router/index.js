e5b54451a72159963463691f3bc0a6e8a9691a14
Delljones <dell_jones@outlook.com>
2022-09-03 01:20:20 +1000

    Update to project

--- assessment_3/src/router/index.js
+++ assessment_3/src/router/index.js
@@ -1,23 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-
+import Home from "@/views/HomeView";
+import About from "@/views/AboutView";
+import Contact from "@/views/ContactView";
+import Blog from "@/views/BlogView";
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  { path: "/", name: "home", component: Home },
+  { path: "/about", name: "about", component: About },
+  { path: "/blog", name: "blog", component: Blog },
+  { path: "/contact", name: "contact", component: Contact },
 ];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
 export default router;
