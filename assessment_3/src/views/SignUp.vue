657b84b1ed84cc6db7574d78b0f5659cf722c298
Delljones <dell_jones@outlook.com>
2022-09-09 13:05:16 +1000

    Update to project

--- assessment_3/src/views/SignUp.vue
+++ assessment_3/src/views/SignUp.vue
@@ -15,6 +15,21 @@
     <label for="">Email: </label>
     <input type="email" v-model="email">
   </div>
+    <br>
+    <br>
+
+    <div>
+      <input type="checkbox" value="First Option" v-model="checkedValues">
+      <label for=""> I would like to be sent promotional emails  </label>
+      <input type="checkbox" value="Second Option" v-model="checkedValues">
+      <label for="" > Please DO NOT send me promotional emails  </label>
+      <br>
+      <br>
+      <H3>Your Response was: </H3>
+
+      {{checkedValues}}
+    </div>
+
   </div>
 
 </template>
